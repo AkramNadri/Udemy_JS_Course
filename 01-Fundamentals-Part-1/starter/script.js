fdb45e27963db2cd00662dcdf70c36aebd62b603
AkramSultani <akram.nadri@gmail.com>
2020-10-23 14:43:06 -0400

    Values and Variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+// Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country.
+
+const country = 'Iran';
+const continent = 'Asia';
+
+console.log(country);
+console.log(continent);
