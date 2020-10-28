84a9a9d398a5a59fe3ad318389691a680e552863
AkramSultani <akram.nadri@gmail.com>
2020-10-28 12:56:35 -0400

    Strict mode added. Strict mode displays errors.

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let hasDriversLicense = false;
+const PassTest = true;
+
+if(PassTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can drive');
+
+// strict mode catches errors
+// const interface = 'Audio';
+// const private = 23;
+// const if = 23;
