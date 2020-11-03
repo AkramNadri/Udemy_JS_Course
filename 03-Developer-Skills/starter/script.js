c9b24e03d7843d593d8a61ee94f80f6a464b4f17
AkramSultani <akram.nadri@gmail.com>
2020-11-03 17:27:51 -0500

    Completed setup developer workflow

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+// const xerer = 23;
+
+// const calcAge = birthYear => 2037 - birthYear;
+
+// console.log(xerer);
+
+// // TODO
