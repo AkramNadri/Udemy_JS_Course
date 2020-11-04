13816a914ca09405ee5bfecb53394a78f8626e58
AkramSultani <akram.nadri@gmail.com>
2020-11-04 13:40:38 -0500

    How to solve any problem in 4 steps

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -8,3 +8,81 @@
 // console.log(xerer);
 
 // // TODO
+
+// 4 STEPS TO SOLVE ANY PROBLEM
+
+/*
+PROJECT MANAGER. "WE NEED A FUNCTION THAT REVERSES WHATEVER 
+WE PASS INTO IT"
+*/
+
+/*
+1. MAKE SURE YOU %100 UNDERSTAND THE PROBLEM. ASK THE RIGHT QUESTIONS TO GET A CLEAR PICTURE OF THE PROBLEM.
+*/
+
+/*
+--  WHAT DOES 'WHATEVER' MEAN ? ONLY STRINGS, NUMBERS AND 
+    ARRAYS MAKE SENSE TO REVERSE.
+
+--  WHAT TO DO IF SOMETHING ELSE IS PASSED IN ?
+
+--  WHAT SHOULD BE RETURNED ?
+
+--  HOW TO RECOGNIZE WHETHER THE ARGUMENT IS A NUMBER, STRING
+    OR ARRAY.
+
+--  HOW TO REVERSE A NUMBER, STRING AND ARRAY ?
+*/
+
+/*
+2. DIVIDE AND CONQUER: BREAK A BIG PROBLEM INTO SMALLER
+SUB-PROBLEMS.
+*/
+
+/*
+--  CHECK IF ARGUMENT IS A NUMBER, STRING OR ARRAY
+
+--  IMPLEMENT REVERSE A NUMBER
+
+--  IMPLEMENT REVERSE AN ARRAY
+
+--  IMPLEMENT REVERSE A STRING
+
+--  RETURN REVERSED VALUE
+*/
+
+/*
+3. DONT BE AFRAID TO DO AS MUCH RESEARCH AS YOU HAVE TO 
+*/
+
+/*
+--  HOW TO CHECK IF A VALUE IS A NUMBER, STRING OR ARRAY
+    IN JAVASCRIPT ?
+
+--  HOW TO REVERSE A NUMBER, STRING OR ARRAY IN JAVASCRIPT ?
+*/
+
+/*
+4. FOR BIGGER PROBLEMS, WRITE PSEUDO-CODE BEFORE WRITING THE ACTUAL CODE
+*/
+
+/*
+
+write speudo-code so that you can understand the problem
+
+
+function reverse(value)
+    if value !string || !number || !array
+    return value
+
+    if value type == string
+    reverse string
+
+    if value type == number
+    reverse number
+
+    if value type == array
+    reverse array
+
+    return reversed value
+*/
