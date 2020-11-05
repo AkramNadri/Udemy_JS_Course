7be23423487494282d8eca531e1793b299512ac5
AkramSultani <akram.nadri@gmail.com>
2020-11-05 14:12:26 -0500

    Game logic - if no guess is made

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,14 +1,25 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-// setting text into .massage class
-document.querySelector('.message').textContent = 'Correct Number!👌';
+// // setting text into .massage class
+// document.querySelector('.message').textContent = 'Correct Number!👌';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-// inputting number 23 into .guess class
-document.querySelector('.guess').value = 23;
-// displaying value of .guess class
-console.log(document.querySelector('.guess').value);
+// // inputting number 23 into .guess class
+// document.querySelector('.guess').value = 23;
+// // displaying value of .guess class
+// console.log(document.querySelector('.guess').value);
+
+// Event handler
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
