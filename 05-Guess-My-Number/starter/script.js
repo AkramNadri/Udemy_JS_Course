0ed6dd2bd316f8392ba178722dc6d27c37eb6627
AkramSultani <akram.nadri@gmail.com>
2020-11-05 13:54:41 -0500

    DOM manipulation using document.querySelector

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent);
+
+// setting text into .massage class
+document.querySelector('.message').textContent = 'Correct Number!👌';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+// inputting number 23 into .guess class
+document.querySelector('.guess').value = 23;
+// displaying value of .guess class
+console.log(document.querySelector('.guess').value);
