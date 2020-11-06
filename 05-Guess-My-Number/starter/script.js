b1794388215900645c735b3f503e8db294a11a0a
AkramSultani <akram.nadri@gmail.com>
2020-11-06 15:48:00 -0500

    game update vers 0.1

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -24,28 +24,35 @@ let score = 20;
 
 document.querySelector('.number').textContent = secretNumber;
 
+// Event listener when "Check!" button is clicked.
+// function const guess can be used as a value
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
 
+  // if no guess is input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+    // if guess equals secret number - user wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!👌';
-    ///////////////////
+    // If guess is greater then the secret number
   } else if (guess > secretNumber) {
-    if (score > 1) {
+    if (score > 0) {
       document.querySelector('.message').textContent = 'Too High! 👆🏻 ';
       score--;
       document.querySelector('.score').textContent = score;
+      // If player score reaches zero, player loses
     } else {
       document.querySelector('.message').textContent = 'You Lose! ❌';
     }
+    // If guess is less then the secret number
   } else if (guess < secretNumber) {
-    if (score > 1) {
+    if (score > 0) {
       document.querySelector('.message').textContent = 'Too Low! 👇🏻';
       score--;
       document.querySelector('.score').textContent = score;
+      // If player score reaches zero, player loses
     } else {
       document.querySelector('.message').textContent = 'You Lose! ❌';
     }
