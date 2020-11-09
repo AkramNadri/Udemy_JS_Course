f9cce46feef2f215c39873e36c7dec17adce80d0
AkramSultani <akram.nadri@gmail.com>
2020-11-09 14:09:14 -0500

    Modal window - Proect #2 working with classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// function to be called on btnCloseModal to add .hidden class on modal & overlay
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// function to be called on btnsOpenModal to remove .hidden class on modal & overlay
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// eventListener to close modal & overlay with .close-modal class
+btnCloseModal.addEventListener('click', closeModal);
+
+// eventListener to close modal & overlap when clicked off modal
+overlay.addEventListener('click', closeModal);
