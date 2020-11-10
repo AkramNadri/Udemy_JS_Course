435cca0ebde0222953a05e0a167b09fd24ec34b9
AkramSultani <akram.nadri@gmail.com>
2020-11-10 13:34:55 -0500

    Hold button logic

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -39,6 +39,25 @@ score1Element.textContent = 0;
 // applying .hidden class to dice element
 diceElement.classList.add('hidden');
 
+// Hold score functionality
+btnHold.addEventListener('click', function () {
+  document.getElementById(`score--${activePlayer}`).textContent = currentScore;
+  currentScore = 0;
+
+  // Ternary operator to switch between #current-- id
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  // toggles class .player--active on/off
+  player0Element.classList.toggle('player--active');
+  player1Element.classList.toggle('player--active');
+
+  if (score0Element >= 30 || score1Element >= 30) {
+    console.log(` player ${activePlayer} wins!`);
+  } else {
+    activePlayer = activePlayer === 0 ? 1 : 0;
+  }
+});
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // generate random dice roll
@@ -54,15 +73,21 @@ btnRoll.addEventListener('click', function () {
     // add dice to current score
     currentScore += dice;
 
+    // selects the #current-- id and places current score into the text content
     document.getElementById(
       `current--${activePlayer}`
     ).textContent = currentScore;
 
-    current0Element.textContent = currentScore; // change later
+    // current0Element.textContent = currentScore; // change later
   } else {
+    // resets the #current-- id to 0 for current active player
     document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+    // Ternary operator to switch between #current-- id
     activePlayer = activePlayer === 0 ? 1 : 0;
     currentScore = 0;
+
+    // toggles class .player--active on/off
     player0Element.classList.toggle('player--active');
     player1Element.classList.toggle('player--active');
   }
