115a82ef0a880825bf2d281ff056af2637d5ce22
AkramSultani <akram.nadri@gmail.com>
2020-11-11 14:16:30 -0500

    Global and local/function scope

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+// function/local scope
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  // function/local scope within calcAge function
+  // has access to variables in calcAge function
+  function printAge() {
+    const output = `${firstName} you are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+
+  printAge();
+
+  return age;
+}
+
+// global scope
+const firstName = 'Akram';
+calcAge(1984);
