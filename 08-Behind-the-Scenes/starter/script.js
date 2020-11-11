e69327cf7e4f47bb864781905a19b987436c6468
AkramSultani <akram.nadri@gmail.com>
2020-11-11 15:28:49 -0500

    Scope

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -7,8 +7,28 @@ function calcAge(birthYear) {
   // function/local scope within calcAge function
   // has access to variables in calcAge function
   function printAge() {
-    const output = `${firstName} you are ${age}, born in ${birthYear}`;
+    let output = `${firstName} you are ${age}, born in ${birthYear}`;
     console.log(output);
+
+    // const and let are blocked scoped, they are only available within the function or block
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      // var type can be access outside the function/block scope
+      var millenial = true;
+      const firstName = 'Steven';
+      const str = `you are a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      output = 'NEW OUTPUT';
+    }
+
+    console.log(output);
+    // var millenial is accesible outside the function or block
+    console.log(millenial);
+    // console.log(add(2, 3));
   }
 
   printAge();
