13e8ff603560a98aa55c08fe4ad358a0d7fb22a3
AkramSultani <akram.nadri@gmail.com>
2020-11-17 15:58:00 -0500

    Coding challenge #1 using REST and SPREAD operators, along with AND operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -57,16 +57,123 @@ const restaurant = {
   },
 };
 
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests ? restaurant.numGuests : 10;
+///////////////////////////////////////
+// Coding Challenge #1
 
-console.log(guests);
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
 
-// ??
-// this operator only works with nullish values
-// Nullish: null and undefined (NOT 0 or '')
-const guestCorret = restaurant.numGuests ?? 10;
-console.log(guestCorret);
+1. Create one player array for each team (variables 'players1' and 'players2')
+
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// 1. create array of all players in team1
+// const player1 = [...game.players[0]];
+// 1. create array of all players in team2
+// const player2 = [...game.players[1]];
+
+const [player1, player2] = game.players;
+console.log(player1, player2);
+
+// 2. first player set to gk, and all remaining players set to fieldPlayers
+const [gk, ...fieldPlayers] = player1;
+console.log(gk, fieldPlayers);
+
+// 3. all players on team1 and team2 on allPlayers
+const allPlayers = [...player1, ...player2];
+console.log(allPlayers);
+
+// 4. all players on team1 into players1Final plus add 'Thiago', 'Coutinho' and 'Perisic'.
+const players1Final = [...player1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+// 5.
+const {
+  odds: { team1: team1, x: draw, team2: team2 },
+} = game;
+
+console.log(` team1=${team1}, draw=${draw}, team2=${team2}`);
+
+// 6. printGoals function takes arbitrary number of names
+const printGoals = function (...playerNames) {
+  console.log(playerNames);
+  console.log(`${playerNames.length} goals were scored`);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+
+printGoals(...game.scored);
+
+// 7. we use the AND operator here because if the first condition is true it will continue through to the last condition and print
+team1 < team2 && console.log('Team 1 likely to win');
+team1 > team2 && console.log('Team 2 likely to win');
+
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests ? restaurant.numGuests : 10;
+
+// console.log(guests);
+
+// // ??
+// // this operator only works with nullish values
+// // Nullish: null and undefined (NOT 0 or '')
+// const guestCorret = restaurant.numGuests ?? 10;
+// console.log(guestCorret);
 
 // console.log(' ------------- OR -----------');
 // // use ANY data type
@@ -106,6 +213,7 @@ console.log(guestCorret);
 
 // restaurant.orderPizza && restaurant.orderPizza('mushrrom', 'spinach');
 
+/******************** SPREAD  ******************/
 // SPREAD, because on the RIGHT side of =
 // const arr = [1, 2, ...[3, 4]];
 
@@ -229,6 +337,7 @@ console.log(guestCorret);
 
 // // creating a new array, use spread operator on mainMenu property and adding a value 'Gnocci' to the end of the new array
 
+/**** */
 // // spread operator takes all the elements in the array and doesnt create new variables. We can only use it in place where we'd otherwise write values seperated by commas.
 // const newMenu = [...restaurant.mainMenu, 'Gnocci'];
 // console.log(newMenu);
