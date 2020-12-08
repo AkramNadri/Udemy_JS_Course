9e9666845cbbd07b97fd608339234fe1649b9cb4
AkramSultani <akram.nadri@gmail.com>
2020-12-08 13:27:45 -0500

    Functions accepting callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,6 +1,49 @@
 'use strict';
 
 ////////////////////////////////////////////////////////////////
+
+// FUNCTIONS ACCEPTING CALLBACK FUNCTIONS
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+upperFirstWord('hello there');
+
+// Higher-order function
+// transformer is the higher-order function
+const transformer = function (str, fn) {
+  console.log(`'original string: ${str}`);
+  // fn is the function passed in the argument and is a callback function
+  console.log(`'Transformed string: ${fn(str)}`);
+  console.log(`'Transformed by: ${fn.name}`);
+};
+
+const highOrderFunction = function (str, fn) {
+  console.log(` this is the ${str}`);
+  console.log(` callback uppFirstWord function ${fn(str)}`);
+};
+
+highOrderFunction('sending to function oneWord', oneWord);
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('The oneWord function', oneWord);
+
+// JS uses callbacks ALL the time
+const high5 = function () {
+  console.log(' !!! ');
+};
+
+document.body.addEventListener('click', high5);
+
+['Akram', 'Martha', 'Adam'].forEach(high5);
+
+////////////////////////////////////////////////////////////////
 // First-class and higher-order functions
 
 /*
