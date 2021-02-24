ddd9ad4e6fc74dc2129478cf7a9360d145d89ffc
AkramSultani <akram.nadri@gmail.com>
2021-02-24 16:44:51 -0500

    Coding challenge #3 - Success

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -186,6 +186,17 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
+const calcAverageHumanAge = ages =>
+  ages
+    .map(ages => (ages <= 2 ? 2 * ages : 16 + ages * 4))
+    .filter(ages => ages >= 18)
+    .reduce((acc, curr, i, arr) => acc + curr / arr.length, 0);
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(avg1, avg2);
+
 ///////////////////////////////////////
 // Coding Challenge #2
 
