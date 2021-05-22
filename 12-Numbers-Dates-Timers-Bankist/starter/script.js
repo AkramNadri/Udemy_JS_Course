b4de5f72508193ce86c4a5f4040866e2c39bd61c
AkramSultani <akram.nadri@gmail.com>
2021-05-22 14:55:56 -0400

    Math and Rounding

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,8 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  // updated so amounts are using Math.floor function to round and handle negative numbers
+  const amount = Math.floor(+inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -254,51 +255,121 @@ btnSort.addEventListener('click', function (e) {
 
 // CONVERTING AND CHECKING NUMBERS
 
-console.log(23 === 23.0);
+// console.log(23 === 23.0);
 
-// Base 10 is 0 - 9
-// Binary base 2 is 0 or 1
+// // Base 10 is 0 - 9
+// // Binary base 2 is 0 or 1
 
-console.log(0.1 + 0.2);
+// console.log(0.1 + 0.2);
 
-console.log(0.1 + 0.2 === 0.3); // false
+// console.log(0.1 + 0.2 === 0.3); // false
 
-// convert string to number
-console.log(Number('23'));
+// // convert string to number
+// console.log(Number('23'));
 
-// the + sign is the same as Number, type coercion
-console.log(+'23');
+// // the + sign is the same as Number, type coercion
+// console.log(+'23');
 
-// Parsing
-// Here we are using base 10 number.
-// JS will parse the number from a String, in order for this to work the string must start with a number.
-// base 10 number
-console.log(Number.parseInt('30px', 10));
+// // Parsing
+// // Here we are using base 10 number.
+// // JS will parse the number from a String, in order for this to work the string must start with a number.
+// // base 10 number
+// console.log(Number.parseInt('30px', 10));
 
-// example - the string here does not start with a number so we get NaN
-// base 10 number
-console.log(Number.parseInt('px30', 10)); // NaN
+// // example - the string here does not start with a number so we get NaN
+// // base 10 number
+// console.log(Number.parseInt('px30', 10)); // NaN
 
-// parseFloat will include decimal
-console.log(Number.parseFloat('2.5rem')); // 2.5
+// // parseFloat will include decimal
+// console.log(Number.parseFloat('2.5rem')); // 2.5
 
-// parseInt does not include decimal
-console.log(Number.parseInt('2.5rem')); // 2
+// // parseInt does not include decimal
+// console.log(Number.parseInt('2.5rem')); // 2
 
-// ** isNaN checks if a value is NOT a number **
-console.log(Number.isNaN(20)); // false since 20 is a number
+// // ** isNaN checks if a value is NOT a number **
+// console.log(Number.isNaN(20)); // false since 20 is a number
 
-console.log(Number.isNaN('20')); // false because this isnt also not a number
+// console.log(Number.isNaN('20')); // false because this isnt also not a number
 
-console.log(Number.isNaN(+'20')); // true
+// console.log(Number.isNaN(+'20')); // true
 
-console.log(Number.isNaN(23 / 0)); // false - infinity
+// console.log(Number.isNaN(23 / 0)); // false - infinity
 
-// ** isFinite ** - better method to check if value is number
-// checks if value is a number
-console.log(Number.isFinite(20)); // true
-console.log(Number.isFinite('20')); // false - since its string
+// // ** isFinite ** - better method to check if value is number
+// // checks if value is a number
+// console.log(Number.isFinite(20)); // true
+// console.log(Number.isFinite('20')); // false - since its string
 
-console.log(Number.isFinite(+'20X')); // false
+// console.log(Number.isFinite(+'20X')); // false
 
-console.log(Number.isFinite(23 / 0)); // false - infinity
+// console.log(Number.isFinite(23 / 0)); // false - infinity
+
+///////////////////////////////////////////////// ******
+
+// MATH AND ROUNDING
+
+// square root
+console.log(Math.sqrt(25));
+
+// same as square root
+console.log(25 ** (1 / 2));
+
+// cube root
+console.log(8 ** (1 / 3));
+
+// find MAX number
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2)); // NaN
+
+// find MIN number
+console.log(Math.min(5, 18, 23, 11, 2));
+
+// Math.PI - calculate radius of circle with 10 pixels
+// calculate are of circle
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// random value between 1 and 6
+console.log(Math.trunc(Math.random() * 6 + 1));
+
+// Math.random will output number between 0 and 1
+// using Math.floor to handle all situations including negative number inputs
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+
+console.log(randomInt(5, 10));
+
+// Rounding integer
+
+// trunc removes and decimal parts
+console.log(Math.trunc(23.3));
+
+// will round to the nearest integer
+console.log(Math.round(23.9));
+
+// round up
+console.log(Math.ceil(23.3)); // 24
+console.log(Math.ceil(23.9)); // 24
+
+// round down
+console.log(Math.floor(23.3)); // 23
+console.log(Math.floor(23.3)); // 23
+
+// negative numbers
+console.log(Math.trunc(-23.3));
+
+// floor works in all situations - wether positive or negative numbers
+console.log(Math.floor(-23.3));
+
+// Rounding decimals
+
+// toFixed will return a String rounded
+console.log((2.7).toFixed(0)); // String 3
+
+console.log((2.7).toFixed(3)); // String 2.700
+
+console.log((2.345).toFixed(2)); // String 2.35
+
+// the + here converts the String back to Number
+// + same as Number method
+console.log(+(2.7).toFixed(2));
