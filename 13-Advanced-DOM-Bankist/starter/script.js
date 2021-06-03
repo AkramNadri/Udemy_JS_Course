4b350fa63c1cd7a8c7643a991938059998acc5db
AkramSultani <akram.nadri@gmail.com>
2021-06-03 14:26:10 -0400

    Selecting, Creating and Deleting Elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -33,3 +33,72 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////****///////////////////// *******************
+
+// SELECTING, CREATING AND DELETING ELEMENTS
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+// Select single element
+// the . 'period' before the name header indicates that this is a class
+const header = document.querySelector('.header');
+// Select all occurances of element
+document.querySelectorAll('.section');
+
+// store this selection in a variable
+const allSections = document.querySelectorAll('.section');
+
+console.log(allSections);
+
+// getElementById - will find specified Id
+// since it is an element we do not require the . 'period'
+document.getElementById('section--1');
+
+// Find all the element with the name of button
+// Will return HTMLCollection list
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+// We dont need the . period, will return all classes with btn
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and Inserting elements
+
+// Creates a DOM element
+const message = document.createElement('div');
+// class cookie-message exists in css file, we assign the css style to message element
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics';
+// we use innerHTML to input text into message element
+// create button to close the element
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// prepend message to header, will display message element above header element.
+// prepend adds the element as the first child
+// header.prepend(message);
+// append last child
+header.append(message);
+
+// cloneNode multiple copies of the same element
+// true means all the child elements will also be copied
+// header.append(message.cloneNode(true));
+
+// before header element
+header.before(message);
+// after header element
+// header.after(message);
+
+// Delete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // new way of removing a node
+    message.remove();
+    // previous way of removing a node
+    // message.parentElement.removeChild(message);
+  });
