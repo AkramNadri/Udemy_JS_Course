384b59ad571255f877e29e87d75bfd45a3d3588f
AkramSultani <akram.nadri@gmail.com>
2021-06-04 14:29:14 -0400

    Implementing smooth scrolling#

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -39,125 +39,178 @@ document.addEventListener('keydown', function (e) {
 // SELECTING, CREATING AND DELETING ELEMENTS
 
 // Selecting elements
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
 
-// Select single element
-// the . 'period' before the name header indicates that this is a class
-const header = document.querySelector('.header');
-// Select all occurances of element
-document.querySelectorAll('.section');
+// // Select single element
+// // the . 'period' before the name header indicates that this is a class
+// const header = document.querySelector('.header');
+// // Select all occurances of element
+// document.querySelectorAll('.section');
 
-// store this selection in a variable
-const allSections = document.querySelectorAll('.section');
+// // store this selection in a variable
+// const allSections = document.querySelectorAll('.section');
 
-console.log(allSections);
+// console.log(allSections);
 
-// getElementById - will find specified Id
-// since it is an element we do not require the . 'period'
-document.getElementById('section--1');
+// // getElementById - will find specified Id
+// // since it is an element we do not require the . 'period'
+// document.getElementById('section--1');
 
-// Find all the element with the name of button
-// Will return HTMLCollection list
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+// // Find all the element with the name of button
+// // Will return HTMLCollection list
+// const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
 
-// We dont need the . period, will return all classes with btn
-console.log(document.getElementsByClassName('btn'));
+// // We dont need the . period, will return all classes with btn
+// console.log(document.getElementsByClassName('btn'));
 
-// Creating and Inserting elements
+// // Creating and Inserting elements
 
-// Creates a DOM element
-const message = document.createElement('div');
-// class cookie-message exists in css file, we assign the css style to message element
-message.classList.add('cookie-message');
-// message.textContent = 'We use cookies for improved functionality and analytics';
-// we use innerHTML to input text into message element
-// create button to close the element
-message.innerHTML =
-  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// // Creates a DOM element
+// const message = document.createElement('div');
+// // class cookie-message exists in css file, we assign the css style to message element
+// message.classList.add('cookie-message');
+// // message.textContent = 'We use cookies for improved functionality and analytics';
+// // we use innerHTML to input text into message element
+// // create button to close the element
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-// prepend message to header, will display message element above header element.
-// prepend adds the element as the first child
-// header.prepend(message);
-// append last child
-header.append(message);
+// // prepend message to header, will display message element above header element.
+// // prepend adds the element as the first child
+// // header.prepend(message);
+// // append last child
+// header.append(message);
 
-// cloneNode multiple copies of the same element
-// true means all the child elements will also be copied
-// header.append(message.cloneNode(true));
+// // cloneNode multiple copies of the same element
+// // true means all the child elements will also be copied
+// // header.append(message.cloneNode(true));
 
-// before header element
-header.before(message);
-// after header element
-// header.after(message);
+// // before header element
+// header.before(message);
+// // after header element
+// // header.after(message);
 
-// Delete elements
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', function () {
-    // new way of removing a node
-    message.remove();
-    // previous way of removing a node
-    // message.parentElement.removeChild(message);
-  });
+// // Delete elements
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     // new way of removing a node
+//     message.remove();
+//     // previous way of removing a node
+//     // message.parentElement.removeChild(message);
+//   });
 
 //////////////****///////////////////// *******************
 
 // Styles
 // to set a style on element, we get element . style then . the property name
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
 
-console.log(message.style.height); // nothing/hidden
-console.log(message.style.backgroundColor);
+// console.log(message.style.height); // nothing/hidden
+// console.log(message.style.backgroundColor);
 
-// this contains all of the properties with all of the values assigned to message element
-// console.log(getComputedStyle(message));
-// getComputedStyle will retrieve info from element
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
+// // this contains all of the properties with all of the values assigned to message element
+// // console.log(getComputedStyle(message));
+// // getComputedStyle will retrieve info from element
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
 
-// increase height on element
-// result of this is a string, here were trying to add number to a string which will not work. Must use Number.parseFloat function to convert string to number.
-// How to parse number out of a string
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+// // increase height on element
+// // result of this is a string, here were trying to add number to a string which will not work. Must use Number.parseFloat function to convert string to number.
+// // How to parse number out of a string
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
-// CSS custom properties
-document.documentElement.style.setProperty('--color-primary', 'orangered');
+// // CSS custom properties
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
 
-// Attributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className); // nav__logo
+// // Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className); // nav__logo
 
-// adding alt to logo class
-logo.alt = 'Beautiful minimalist logo';
+// // adding alt to logo class
+// logo.alt = 'Beautiful minimalist logo';
 
-// Non standard
-// Here we add designer class to logo
-console.log(logo.designer); // undefined
-console.log(logo.getAttribute('designer')); // Akram
+// // Non standard
+// // Here we add designer class to logo
+// console.log(logo.designer); // undefined
+// console.log(logo.getAttribute('designer')); // Akram
 
-// new attribute created
-logo.setAttribute('company', 'Bankist');
+// // new attribute created
+// logo.setAttribute('company', 'Bankist');
 
-console.log(logo.getAttribute('src')); // Relative version
-console.log(logo.src); // Absolute version
+// console.log(logo.getAttribute('src')); // Relative version
+// console.log(logo.src); // Absolute version
 
-// get absolute path
-const link = document.querySelector('.twitter-link');
-console.log(link.href); // absolute
-console.log(link.getAttribute('href')); // absolute
+// // get absolute path
+// const link = document.querySelector('.twitter-link');
+// console.log(link.href); // absolute
+// console.log(link.getAttribute('href')); // absolute
 
-// Data attributes
-console.log(logo.dataset.versionNumber);
+// // Data attributes
+// console.log(logo.dataset.versionNumber);
 
-// Classes
-logo.classList.add('c');
-logo.classList.remove('c');
-logo.classList.toggle('c');
-logo.classList.contains('c');
+// // Classes
+// logo.classList.add('c');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
+
+//////////////****///////////////////// *******************
+
+// IMPLEMENTING SMOOTH SCROLLING
+
+const buttonScrollTo = document.querySelector('.btn--scroll-to');
+
+// # is used for ID: id="section--1
+const section1 = document.querySelector('#section--1');
+
+buttonScrollTo.addEventListener('click', function (e) {
+  // get coords of the section we want to scroll to
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  // relative to the viewpoint
+  console.log(e.target.getBoundingClientRect());
+
+  // current scroll position
+  // current position relative to the top of the page
+  // X = horizontal scroll
+  // Y = vertical scroll
+  console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+
+  // Read the height and the width of the viewport
+  // will change depending on size of current window
+  // We use coords to tell JS where to scroll to
+  console.log(
+    'Height/Width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+  window.scrollTo(
+    // current position + current scroll
+    s1coords.left + window.pageXOffset,
+    // current position + current scroll
+    s1coords.top + window.pageYOffset
+  );
+
+  // oldschool way
+  // passing an object
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+
+  // Modern way
+  // Only works with modern browsers
+  // section1.scrollIntoView({ behavior: 'smooth' });
+});
