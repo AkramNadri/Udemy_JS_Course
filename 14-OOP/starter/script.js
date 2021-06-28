a34efebb1295ba12e25d70c6340fe864025a3a53
AkramSultani <akram.nadri@gmail.com>
2021-06-28 17:02:26 -0400

    Prototype

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-// CONSTRUCTOR FUNCTION AND THE NEW OPERATOR
+// CONSTRUCTOR FUNCTION AND THE NEW OPERATOR ----
+
 // A constructor is a completely normal function, the only difference between a regular function and a constructor function is the we call a constructor function with the 'new' operator.
 
 // Constructor functions always start with a capital letter.
@@ -31,9 +32,64 @@ console.log(jonas);
 
 // 4. function automatically returns {} 'object'.
 // --------------------------------------------------------
+// --------------------------------------------------------
 
 const matilda = new Person('Matilda', 2044);
 const bill = new Person('Bill', 2022);
 console.log(matilda, bill);
 
 console.log(jonas instanceof Person);
+
+// --------------------------------------------------------
+// --------------------------------------------------------
+// --------------------------------------------------------
+
+// PROTOTYPES ----
+
+// Each and every function in JS automatically has a property called prototype, and that includes constructor function. Every object thats created by a certain constructor function will get access to all the methods and properties that we define on the constructor prototype property.
+
+//
+console.log(Person.prototype);
+
+// instead of creating a new function within each object creation, we can instead re-use this function calcAge within the object itself. Reusability.
+
+// The 'this' keyword is set to the object thats calling the method.
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+// since 'jonas' is an instance of Person, jonas has access to methods on Person. jonas has access to calcAge.
+// We have access to it since prototypal inheritance
+jonas.calcAge();
+
+// This is the prototype of jonas
+// The prototype of jonas object is essentially the prototype property of the constructor function
+console.log(jonas.__proto__);
+
+console.log(jonas.__proto__ === Person.prototype); // true
+
+// Check if one prototype is the same as another
+console.log(Person.prototype.isPrototypeOf(jonas)); // true
+console.log(Person.prototype.isPrototypeOf(matilda)); // true
+
+// This is not the prototype of Person
+console.log(Person.prototype.isPrototypeOf(Person)); // false
+
+// How is jonas object connected to Person constructor ???
+// During step 3 of creating an object with Person constructor, a link is created between the object and constructor, we can see that link in 'console.log(Person.prototype.isPrototypeOf(jonas));' // true
+
+// Person and jonas object share the same prototype
+// It sets the proto property on the object to the proto property of the constructor function, this is how JS knows they are connected.
+
+// We can also set properties on the prototype, not just methods.
+// This property will now be in each object under __proto__
+Person.prototype.species = 'Homosapiens';
+
+// these objects now have a property called species in __proto__
+console.log(jonas, matilda);
+
+// Check if a property exists in an object
+console.log(jonas.hasOwnProperty('firstName')); // true
+
+// false since species is not directly in the object, it simply has access to it becuase of its prototype
+console.log(jonas.hasOwnProperty('species')); // false
