2c3c935f7d65e2d0964092b85f904f2548a7af99
AkramSultani <akram.nadri@gmail.com>
2021-07-28 17:11:26 -0400

    Coding challenge 3 complete

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -525,9 +525,66 @@ console.log(akram instanceof Object); // true
 
 /* 
 1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+
 2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+
 3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+
 4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism 😉
 DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
 GOOD LUCK 😀
 */
+
+// 1.
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+const Ev = function (make, speed, charge) {
+  // this will point to Car object
+  Car.call(this, make, speed);
+
+  this.charge = charge;
+};
+
+// Set prototype of Ev to Car, Car is parent class and Ev is child class
+Ev.prototype = Object.create(Car.prototype);
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+// 2.
+Ev.prototype.chargeBattery = function (chargeTo) {
+  //
+  this.charge = chargeTo;
+};
+
+// 3.
+Ev.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(
+    `${this.make} going at ${this.speed}, with a charge of %${this.charge}`
+  );
+};
+
+// 4. Create new Ev object
+const tesla = new Ev('Tesla', 120, 23);
+
+//
+Ev.prototype.constructor = Ev;
+
+console.log(tesla);
+tesla.chargeBattery(90);
+console.log(tesla);
+
+tesla.accelerate();
+tesla.accelerate();
+tesla.brake();
+tesla.accelerate();
+tesla.accelerate();
+tesla.accelerate();
+
+console.log(tesla);
