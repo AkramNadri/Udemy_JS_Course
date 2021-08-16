d04c0268c474208466ff08a7f8430c9c0696aadd
AkramSultani <akram.nadri@gmail.com>
2021-08-16 16:54:37 -0400

    Using geolocation API

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,26 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// --------------------------------------------------------
+// --------------------------------------------------------
+// --------------------------------------------------------
+
+// USING THE GEOLOCATION API
+
+// getCurrentPosition takes 2 callback functions, the first one is the callback function called on success, whenever the browser successfully gets the coordinates of the current position of the user. The second is the error callback, happens when an error occurs while trying to get users current location.
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    // get current position function
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+
+    // error function
+    function () {
+      alert('Could not get your position');
+    }
+  );
