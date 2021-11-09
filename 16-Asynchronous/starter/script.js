3fddb42e57aefdc1910d0005259487614a79c47f
AkramSultani <akram.nadri@gmail.com>
2021-11-09 15:45:48 -0500

    problems with callback heck

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,8 +4,92 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+///////////////////////////////////////
 
-const getCountryData = function (country) {
+// updating function to get country and neighbouring country data
+// const getCountry = function (country) {
+//   const request = new XMLHttpRequest();
+
+//   // Initializes a request.
+//   // The HTTP request method to use, such as "GET", "POST", "PUT", "DELETE", etc. Ignored for non-HTTP(S) URLs.
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+
+//   // here we send the request, and this request fetches the data in the backgroundß
+//   request.send();
+
+//   // once send is done it will emit the load event, using the eventListener we are waiting for that event as soon as the data arrives this callback function will be called
+//   request.addEventListener('load', function () {
+//     // the data we received from above request comes in the form of JSON. Need to convert JSON to JS object
+//     // adding [] to data will destructure the object
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     const html = `
+//   <article class="country">
+//   <img class="country__img" src="${data.flags.png}" />
+//   <div class="country__data">
+//     <h3 class="country__name">${data.name.common}</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>👫</span>${(
+//       +data.population / 1000000
+//     ).toFixed(1)}/million people</p>
+//     <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
+//     <p class="country__row"><span>💰</span>${data.currencies}</p>
+//   </div>
+// </article>`;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+
+// // sometimes the countries will not load in order because it depends on which data is retrieved first
+// getCountryData('portugal');
+// getCountryData('canada');
+// getCountryData('america');
+
+// How the web works
+// TCP/IP connection, 3 way handshake, how data travels across the internet, multiple packets are sent across the internet and TCP rearranges the packets back into its original form/respone.
+
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+
+// Welcome to callback Heck
+
+const renderCountry = function (data, className = '') {
+  //
+  const html = `
+  <article class=${className}>
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}/million people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
+    <p class="country__row"><span>💰</span>${data.currencies}</p>
+  </div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+
+  countriesContainer.style.opacity = 1;
+};
+
+// One callback function inside another callback function
+// below is an example of callback heck, because what if we needed data on many countries, we would have to put another callback function. This leads to many callback functions within other callback functions and we can only receive data on the other callbacks when the first callback is completed.
+
+// CALLBACK HELL IS WHEN WE HAVE ALOT OF NESTED CALLBACKS IN ORDER TO EXECUTE ASYNCHRONUS TASKS IN SEQUENCE.
+
+// CALLBACK HELL is bad because it makes our code look very messy, but even more important is it makes our code hard to understand and reason about.
+
+// We can escape callback heck by using "Promises".
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
   const request = new XMLHttpRequest();
 
   // Initializes a request.
@@ -22,29 +106,32 @@ const getCountryData = function (country) {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
 
-    const html = `
-  <article class="country">
-  <img class="country__img" src="${data.flags.png}" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name.common}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      +data.population / 1000000
-    ).toFixed(1)}/million people</p>
-    <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
-    <p class="country__row"><span>💰</span>${data.currencies}</p>
-  </div>
-</article>`;
+    // render country 1
+    renderCountry(data);
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
+    // get neighbour country (2)
+    // since the data we get back can contain multiple borders and not just 1 we need to destructure neighbour
+    const [neighbour] = data.borders;
 
-    countriesContainer.style.opacity = 1;
+    // if there is a country with no neighbour, gaurd clause
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    // Firing off the second AJAX
+    // this request will always be fired after the first request. Since above is eventListener load comes first, this event will be executed right after.
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      //
+
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-// sometimes the countries will not load in order because it depends on which data is retrieved first
-getCountryData('portugal');
-getCountryData('canada');
-getCountryData('america');
-
-// How the web works
+getCountryAndNeighbour('canada');
